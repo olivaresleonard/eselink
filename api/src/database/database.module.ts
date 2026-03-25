ec586a1da4027b7b8494832ec915d9e03b2420c8
Leonard Olivares <me@olivaresleonard.com>
2026-03-25 20:07:12 -0300

    fix docker

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -9,23 +9,27 @@ import {TypeOrmModule} from '@nestjs/typeorm';
     ConfigModule,
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: Number(configService.get<string>('DB_PORT', '5432')),
-        username: configService.get<string>(
-          'DB_USERNAME', 'postgres'
-        ),
-        password: configService.get<string>(
-          'DB_PASSWORD', 'postgres'
-        ),
-        database: configService.get<string>(
-          'DB_DATABASE', 'eselink'
-        ),
-        ssl: configService.get<string>('DB_SSL', 'false') === 'true',
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const useSsl = configService.get<string>('DB_SSL', 'false') === 'true';
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST', 'localhost'),
+          port: Number(configService.get<string>('DB_PORT', '5432')),
+          username: configService.get<string>(
+            'DB_USERNAME', 'postgres'
+          ),
+          password: configService.get<string>(
+            'DB_PASSWORD', 'postgres'
+          ),
+          database: configService.get<string>(
+            'DB_DATABASE', 'eselink'
+          ),
+          ssl: useSsl ? {rejectUnauthorized: false} : false,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
   ],
   exports: [TypeOrmModule],
